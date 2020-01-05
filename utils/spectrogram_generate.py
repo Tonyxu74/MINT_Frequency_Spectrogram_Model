5b5754d2927d528257bebda41710fd074e21d49d
Tonyxu74 <tonyxu74@hotmail.com>
2020-01-05 14:25:19 -0700

    completed spectogram_generate with null data

--- utils/spectrogram_generate.py
+++ utils/spectrogram_generate.py
@@ -3,6 +3,7 @@ from glob import glob
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 
 def gen_spectrogram():
@@ -10,9 +11,17 @@ def gen_spectrogram():
     raw_path_list = glob("../data/raw/*/")
 
     for raw_path in raw_path_list:
+
         # find raw data and logs separately
         data_paths = glob(raw_path + "raw*.txt")
         label_paths = glob(raw_path + "log*.txt")
+
+        # make training folders IF they do not already exist
+        if not os.path.exists(raw_path.replace('raw', 'train')):
+            os.mkdir(raw_path.replace('raw', 'train'))
+
+        # keep track of how many data points we generate PER experimentee
+        num_data_points = 0
 
         for data_path, label_path in zip(data_paths, label_paths):
             # open each txt file
@@ -29,12 +38,21 @@ def gen_spectrogram():
                     continue
                 label_val, t_in_vid, t_of_clip = line.split(' ')
                 label_list.append({
-                    'label': int(label_val),
+                    'label': int(label_val) + 1,
                     'time': int(t_in_vid),
                     'length': int(t_of_clip.replace('\n', ''))
                 })
 
                 lines_read += 1
+            # get some null values as well (guess that nothing is happening, occurs in gaps between labels)
+            prev_time = 0
+            for i in range(len(label_list)):
+                label_list.append({
+                    'label': 0,
+                    'time': prev_time,
+                    'length': label_list[i]['time'] - prev_time
+                })
+                prev_time = label_list[i]['time'] + label_list[i]['length']
 
             # === extracting raw data info ===
             lines_read = 0
@@ -98,10 +116,21 @@ def gen_spectrogram():
 
                 data_arr = np.asarray(data_clip)
                 num_channels = 8
-                for cha in range(num_channels):
-                    f, t, spectrogram = signal.spectrogram(data_arr[:, cha], fs=250, nperseg=25)
 
-                    # now save spectrogram
+                # generate and append spectrograms of each channel
+                total_spectrograms = []
+                for cha in range(num_channels):
+                    # use n per seg of 22 to have a shape of 12x12
+                    f, t, spectrogram = signal.spectrogram(data_arr[:, cha], fs=250, nperseg=22)
+                    total_spectrograms.append(spectrogram)
+
+                # finally convert to array and save
+                total_spectrograms = np.asarray(total_spectrograms)
+                save_path = raw_path.replace('raw', 'train') + '{}.npy'.format(num_data_points)
+                np.save(save_path, total_spectrograms)
+
+                # we have saved a new datapoint, this increment this by one
+                num_data_points += 1
 
 
 if __name__ == "__main__":
