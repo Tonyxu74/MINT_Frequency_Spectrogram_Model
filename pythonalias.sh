9ec411d46211ab487f023b7235c401efdad6833a
Alex Wen <alexwenemail@gmail.com>
2020-09-27 09:58:50 -0700

    Delete pythonalias.sh

--- pythonalias.sh
+++
@@ -1,5 +0,0 @@
-# I need this for my system to point to the python installation i want to use. 
-# You might not need it, ensure that your packages and installations are organized! 
-
-alias python3=/mnt/c/Anaconda/python.exe
-export PYTHONPATH="/mnt/c/Anaconda/Lib/site-packages"
